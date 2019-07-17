a1029922a46353c3a8512b2a335cc4181e191bc8
Jason Shockness <jshock12@gmail.com>
2019-07-17 14:29:00 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -52,7 +52,7 @@ class Person
   def call_friend(friend)
     self.happiness += 3
     friend.happiness += 3
-    "Hi #{self.name}! It's #{friend.name}. How are you?"
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
   end
 end
 
